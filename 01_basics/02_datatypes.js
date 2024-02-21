d17d412e8ff75d13a53837cdeae3571189b7f9d3
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-02-21 12:27:07 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"   //treat all JS code as newer version
+
+//window.alert(3 + 3) //we are using node.js not the browser it is only allowed in browser
+
+//Datatypes in javascript
+
+let num = 134
+let name = "Anurag"
+let state = true
+
+//number
+//bigInt => long in java 
+//string = ""
+//null  here null is standalone a type
+//undefined => means kuchh define nhi kiya
+
+
+//object 
+
+console.log(typeof "Anurag"); // string type
+
+console.log(typeof 8); //number type
+
+console.log(typeof null);
+console.log(typeof undefined);
