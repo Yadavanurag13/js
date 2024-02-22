2116f81d9dc9f149646ba4d8729c324f49e41544
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-02-22 13:12:37 +0000

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,12 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//comparison convert the null into 0 and then compare with the number whereas equlity operator do not
+
+//conversion happens here
+console.log("2" == 2);
+
+// strict comparison check the data type and value
+console.log("2" === 2);
+
