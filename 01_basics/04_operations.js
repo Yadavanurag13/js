2ca7654c5c44dc35c5f821f314d641f49ce15c5d
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-02-22 13:02:58 +0000

    operations done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,18 @@
+let value = 3
+
+let negValue = -value
+
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Anurag"
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+console.log(typeof("1" + 2));
+console.log(typeof(2 + "1"));
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+console.log(((3 + 4) * 5) % 3);
