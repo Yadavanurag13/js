20db311abab529b7789756a860f60a1d380680c9
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-02-27 20:22:24 +0000

    scope basics

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,15 @@
+
+
+
+if(true) {
+    let a = 10
+    const b = 20
+    var c = 30  
+}
+
+//console.log(a);
+//console.log(b);
+
+
+//here is problem var is comming out of the scope and printing value this is not good 
+console.log(c);
