71b4a37081dfb7c1ee6b66da8e6a6ba47271f438
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-02-28 11:51:07 +0000

    scopeee donee

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -12,4 +12,48 @@ if(true) {
 
 
 //here is problem var is comming out of the scope and printing value this is not good 
-console.log(c);
+// console.log(c);
+
+
+function one() {
+    const userName = "anurag"
+
+    function two() {
+        const website = "youtube"
+        //userName is accessbile in this function
+        //console.log(userName);
+    }
+    //website is not accessible here outside of scope
+    //console.log(website);
+
+    two();
+}
+one();
+
+// console.log("hello");
+
+
+if(true) {
+    const userName = "Anurag"
+    if(userName == "Anurag") {
+        const website = "youtube"
+
+        //console.log(userName + website);
+    }
+    console.log(userName);
+}
+
+
+function addOne(num) {
+    return num + 1
+}
+
+addOne(5)
+
+// declear hone ke baad variable me rakha h
+// to usee 
+const addTwo = function() {
+    return num + 2
+}
+//we can not addTwo function upper of that function 
+addTwo(2)
