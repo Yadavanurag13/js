9c27914fc688d8500b1591f7721cf929f6129a02
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-02-29 15:22:49 +0000

    iffe

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,22 @@
+// iife function is used to prevent form global polution
+
+// immediately invoked function expressions
+//()()
+
+
+(function anurag() {
+
+    //named iife
+    console.log(`DB connected`);
+})();
+
+//we need to stop function explicitely this time by adding ;
+
+// (() => {
+//     console.log(`DB connected`);
+// }) ()
+//every function will work out wheather it is regular funtion or arrow function
+
+( (name) => {
+    console.log(`DB connected two ${name}`);
+} ) ("anurag")
