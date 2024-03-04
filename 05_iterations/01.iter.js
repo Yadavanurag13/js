39a9674a8ec08ebe59f3c37aa887102ceb5a89d5
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-03-04 14:12:32 +0000

    for of and for in loop done

---
+++ 05_iterations/01.iter.js
@@ -0,0 +1,74 @@
+//for fo loop
+
+// [{}, {}]
+
+//kind of for each loop of java
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+
+for (const i of arr) {
+    //console.log(i);
+}
+
+const greeting = "hello world"
+
+for(const greet of greeting) {
+    if(greet == ' ') {
+        continue;
+    }else {
+        //console.log(`each char of greeting ${greet}`);
+    }
+}
+
+const map = new Map()
+map.set('In', "India")
+map.set('USA', "United States of America")
+
+console.log(map)
+
+for(const key of map) {
+    //console.log(key);
+}
+
+for(const [key] of map) {
+    //console.log(key);
+}
+
+for(const [key, value] of map) {
+    //console.log(key, value);
+}
+
+const myObject = {
+    'game1': 'bgmi', 
+    'game2': 'pubg'
+}
+
+// for(const key of myObject) {
+//     console.log(key);
+// }
+
+
+//Objects are not iterable by for of loop
+
+const myObjectN = {
+    'js': 'Javascript',
+    'cpp': 'c++',
+    'rb': 'ruby'
+}
+
+for(const key in myObjectN) {
+   // console.log(key);
+}
+
+for(const key in myObjectN) {
+    //console.log(myObjectN[key]);
+}
+
+
+const narr =['js', 'java', 'cpp']
+
+for(const key in narr) {
+    console.log(key);
+}
+
+//map are not iterable so we can not use for in loop for then
